7d54c5d912809846b7bce685680712c0a952685a
Ivan test <ivan02081983s@ya.ru>
2022-11-24 08:03:27 +0300

    fixing variable names

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,21 +1,21 @@
 public class CreditPaymentService {
-    public int calculate(int a, int b) {
+    public int calculate(int creditAmount, int creditTerm) {
         double result;
         int resultInt = 0;
         double bid = 9.99 / 12 / 100; // 9.99% ставка
-        if (b == 1) {
+        if (creditTerm == 1) {
             double degree = Math.pow((1 + bid), -12);
-            result = a * bid / (1 - degree);
+            result = creditAmount * bid / (1 - degree);
             resultInt = (int) result;
         }
-        if (b == 2) {
+        if (creditTerm == 2) {
             double degree = Math.pow((1 + bid), -24);
-            result = a * bid / (1 - degree);
+            result = creditAmount * bid / (1 - degree);
             resultInt = (int) result;
         }
-        if (b == 3) {
+        if (creditTerm == 3) {
             double degree = Math.pow((1 + bid), -36);
-            result = a * bid / (1 - degree);
+            result = creditAmount * bid / (1 - degree);
             resultInt = (int) result;
         }
         return resultInt;
