1ee89ad677669235eec70021f49c0daa9770df56
Ivan test <ivan02081983s@ya.ru>
2022-11-29 07:47:55 +0300

    task adjustment

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,23 +1,11 @@
 public class CreditPaymentService {
     public int calculate(int creditAmount, int creditTerm) {
         double result;
-        int resultInt = 0;
+        int resultInt;
         double bid = 9.99 / 12 / 100; // 9.99% ставка
-        if (creditTerm == 1) {
-            double degree = Math.pow((1 + bid), -12);
-            result = creditAmount * bid / (1 - degree);
-            resultInt = (int) result;
-        }
-        if (creditTerm == 2) {
-            double degree = Math.pow((1 + bid), -24);
-            result = creditAmount * bid / (1 - degree);
-            resultInt = (int) result;
-        }
-        if (creditTerm == 3) {
-            double degree = Math.pow((1 + bid), -36);
-            result = creditAmount * bid / (1 - degree);
-            resultInt = (int) result;
-        }
+        double degree = Math.pow((1 + bid), -(creditTerm * 12));
+        result = creditAmount * bid / (1 - degree);
+        resultInt = (int) result;
         return resultInt;
     }
 }
